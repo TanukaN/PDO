109b4be6027ad8d1840c128f0374b06d55a7bb2a
Tanuka Nayak <tn76@afsconnect1.njit.edu>
2017-11-05 18:12:01 -0500

    added htmlTags.php

---
+++ htmlTags.php
@@ -0,0 +1,13 @@
+<?php
+    class htmlTags {				//html tags used in the project
+        static public function tableFormat() {
+            echo "<table cellpadding='5px' border='1px' style='border-collapse: collapse'>";
+        }
+        static public function tableContent($text) {
+            echo '<td>'.$text.'</td>';
+        }
+        static public function breakTableRow() {
+            echo '</tr>';
+        }
+    }
+?>
