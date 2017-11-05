16d07e2f0916bb7e9f0b4f254de8bf2ba9eb9efa
Tanuka Nayak <31719735+TanukaN@users.noreply.github.com>
2017-11-05 18:22:31 -0500

    Updated PDO.php

--- PDO.php
+++ PDO.php
@@ -1,40 +1,40 @@
 <?php
 
-ini_set('display_errors', 'On');				//Debugging messages set to 'ON'
-error_reporting(E_ALL);
+    ini_set('display_errors', 'On');				//Debugging messages set to 'ON'
+    error_reporting(E_ALL);
 
-class Manage {						//Defining a class with autoload function to attempt to load undefined class
-    public static function autoload($class) {
-        include $class . '.php';
-    }
-}
-spl_autoload_register(array('Manage', 'autoload'));
-
-define('SERVERNAME', 'sql2.njit.edu');
-define('USERNAME','tn76');
-define('PASSWORD','DblDTPzb');
-define('DBNAME','tn76');
-
-try {
-    $conn = new PDO('mysql:host=' . SERVERNAME . ';dbname=' . DBNAME, USERNAME, PASSWORD);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully" . "</br>";
-
-    $stmt = $conn->prepare("select * from accounts where id < 6;");
-    $stmt->execute();
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    $result = $stmt->fetchAll();
-    echo "No. of records is " . $stmt->rowCount() . "<br>";
-
-    htmlTags::tableFormat();
-    foreach ($result as $data) {
-        foreach ($data as $value) {
-            htmlTags::tableContent($value);
+    class Manage {						//Defining a class with autoload function to attempt to load undefined class - in this case htmlTags class
+        public static function autoload($class) {
+            include $class . '.php';
         }
-        htmlTags::breakTableRow();
     }
-}
-catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage() . "</br>";
-}
+    spl_autoload_register(array('Manage', 'autoload'));
+
+    define('SERVERNAME', 'sql2.njit.edu');
+    define('USERNAME','tn76');
+    define('PASSWORD','DblDTPzb');
+    define('DBNAME','tn76');
+
+    try {
+        $conn = new PDO('mysql:host=' . SERVERNAME . ';dbname=' . DBNAME, USERNAME, PASSWORD);   //try statement to test database connection
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully" . "</br>";
+
+        $stmt = $conn->prepare("select * from accounts where id < 6;");                    //Preparing SQL statement with DB connection
+        $stmt->execute();   
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $result = $stmt->fetchAll();
+        echo "No. of records is " . $stmt->rowCount() . "<br>";                    //Display no. of rows returned by the fired SQL query
+
+        htmlTags::tableFormat();                                                    //Display data in a tabular format - htmlTags class called for table tags
+        foreach ($result as $data) {
+            foreach ($data as $value) {
+                htmlTags::tableContent($value);                                      
+            }
+            htmlTags::breakTableRow();
+        }
+    }
+    catch (PDOException $e) {                                                       //catch statement to handle any exception
+        echo "Connection failed: " . $e->getMessage() . "</br>";
+    }
 ?>
