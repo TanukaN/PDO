1ca183d494f69cb1feb283054f58d079941d3979
Tanuka Nayak <31719735+TanukaN@users.noreply.github.com>
2017-11-05 23:53:40 -0500

    Update htmlTags.php

--- htmlTags.php
+++ htmlTags.php
@@ -1,7 +1,6 @@
 <?php
     class htmlTags {				//html tags used in PDO assignment
         static public function tableFormat() {
-        static public function tableFormat() {
             echo "<table cellpadding='5px' border='1px' style='border-collapse: collapse'>";
         }
         static public function tableContent($text) {
